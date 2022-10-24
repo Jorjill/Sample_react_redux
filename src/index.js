b1359cc44ae05365991053294f077a67d0a455e0
John Jorjill <orgil9506@gmail.com>
2022-10-24 01:00:55 -0500

    Revert "Updated package-lock.json"
    This reverts commit a48d3084fcd44b6be896fe9e21ea38f2f3b82feb.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store, persistor } from './state/store';
+import { store, persistor } from './state/store'
 import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/lib/integration/react';
 
